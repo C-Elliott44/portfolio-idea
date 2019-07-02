b4603ae3b125fb44479d63fc060f01e8cdc34974
C-Elliott44 <cameronjohnelliott@gmail.com>
2019-07-02 12:54:17 -0700

    mock up of my portfolio, simple. easy to make mobile, and needs editing still.... need to add projects and media queries

---
+++ javascript/email.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#contact-form").submit(function(e){
+        e.preventDefault();
+        var $form=$(this);
+        $.post($form.attr("action"),$form.serialize()).then(function(){
+            alert("Thank you! I'll be in touch shortly!");
+            $("#contact-form")[0].reset()
+        })
+    })
+});
