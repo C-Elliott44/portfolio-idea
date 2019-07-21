607b52d0c40c543cd216940c20b8873ac0bc4b18
C-Elliott44 <C.Elliott.Dev@Gmail.com>
2019-07-21 13:51:06 -0700

    Big file movement

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.listen(process.env.PORT || 4000, function(){
     console.log('Your node js server is running');
